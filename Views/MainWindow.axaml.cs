0ef727fd0044496c693e9926661cdcffb3e4f1de
敬洪波 <jinghongbo@live.com>
2020-07-08 21:07:38 +0800

    升级框架

--- Views/MainWindow.axaml.cs
+++ Views/MainWindow.axaml.cs
@@ -10,7 +10,7 @@ namespace AriaLauncher.Views
         {
             InitializeComponent();
 #if DEBUG
-            this.AttachDevTools();
+            ///this.AttachDevTools();
 #endif
         }
 
